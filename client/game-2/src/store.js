1ba7d19029e7fbb2510ca07fd09ad6ccddb8a7aa
saner <huhhz@hotmail.com>
2019-12-31 14:31:02 +0800

    test

--- client/game-2/src/store.js
+++ client/game-2/src/store.js
@@ -4,11 +4,7 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    state: {
-
-    },
-    mutations: {
-
-    },
+    state: {},
+    mutations: {},
     actions: {}
 })
